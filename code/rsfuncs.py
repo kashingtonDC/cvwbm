57dd20f2f7d16030ae32ad2adc0d0ed81e67581d
aa_imac <aakash@steves-iMac.local>
2019-08-21 14:30:28 -0700

    update rsfuncs

--- code/rsfuncs.py
+++ code/rsfuncs.py
@@ -12,12 +12,10 @@ from dateutil.relativedelta import relativedelta
 from shapely.ops import unary_union
 from pandas.tseries.offsets import MonthEnd
 
-ee.Initialize()
-
 
 def gdf_to_ee_poly(gdf):
 
-	t = gdf.geometry.simplify(0.1)
+	t = gdf.geometry.simplify(0.01)
 	lls = t.geometry.iloc[0]
 	x,y = lls.exterior.coords.xy
 	coords = [list(zip(x,y))]
@@ -37,138 +35,216 @@ def gdf_to_ee_multipoly(gdf):
 
 	return ee.Geometry.MultiPolygon(multipoly)
 
+def get_area(gdf, fast = True):
+    
+    t = gdf.buffer(0.001).unary_union
+    d  = gp.GeoDataFrame(geometry=gp.GeoSeries(t))
+    if fast:
+        d2  = gp.GeoDataFrame(geometry=gp.GeoSeries(d.simplify(0.001))) 
+        area = gdf_to_ee_multipoly(d2)
+    else:
+        area = gdf_to_ee_multipoly(d)
+        
+    return area
+
+
+
 def get_data(dataset, year, month, area):
-    '''
-    calculates the monthly sum a dataset in units of mm/time 
-    '''
+	'''
+	calculates the monthly sum a dataset in units of mm/time 
+	'''
 
-    col = dataset[0]
-    var = dataset[1]
-    scaling_factor = dataset[2]
+	col = dataset[0]
+	var = dataset[1]
+	scaling_factor = dataset[2]
 
-    t = col.filter(ee.Filter.calendarRange(year, year, 'year')).filter(ee.Filter.calendarRange(month, month, 'month')).select(var).filterBounds(area).sum()
-    t2 = t.multiply(1e-3).multiply(ee.Image.pixelArea()).multiply(scaling_factor).multiply(1e-9)
-    # convert mm to m, multiply by pixel area (m^2), multiply by scaling factor, m^3 to km^3
-    scale = t2.projection().nominalScale()
-    sumdict  = t2.reduceRegion(
-        reducer = ee.Reducer.sum(),
-        geometry = area,
-        scale = scale)
-    
-    result = sumdict.getInfo()[var]
-    
-    return result
+	t = col.filter(ee.Filter.calendarRange(year, year, 'year')).filter(ee.Filter.calendarRange(month, month, 'month')).select(var).filterBounds(area).sum()
+	t2 = t.multiply(1e-3).multiply(ee.Image.pixelArea()).multiply(scaling_factor).multiply(1e-9)
+	# convert mm to m, multiply by pixel area (m^2), multiply by scaling factor, m^3 to km^3
+	scale = t2.projection().nominalScale()
+	sumdict  = t2.reduceRegion(
+		reducer = ee.Reducer.sum(),
+		geometry = area,
+		scale = scale)
+	
+	result = sumdict.getInfo()[var]
+	
+	return result
 
 def monthly_sum(dataset, years, months, area):
-    
-    '''
-    Wrapper for `get_data` that takes a dataset and an area
-    '''
-    monthly = []
+	
+	'''
+	Wrapper for `get_data` that takes a dataset and an area
+	'''
+	monthly = []
 
-    for year in years:
-        print(year)
-        for month in months:
-            r = get_data(dataset, year, month, area)
-            monthly.append(r)
-            time.sleep(5)
-    
-    print("wrapper complete")
-    return monthly
+	for year in years:
+		print(year)
+		for month in months:
+			r = get_data(dataset, year, month, area)
+			monthly.append(r)
+			time.sleep(5)
+	
+	print("wrapper complete")
+	return monthly
 
 def gen_polys(geometry, dx=0.5, dy=0.5):
-    
-    '''
-    Return ee.ImaceCollection of polygons used to submit full res queries
-    '''
-    
-    bounds = ee.Geometry(geometry).bounds()
-    coords = ee.List(bounds.coordinates().get(0))
-    ll = ee.List(coords.get(0))
-    ur = ee.List(coords.get(2))
-    xmin = ll.get(0)
-    xmax = ur.get(0)
-    ymin = ll.get(1)
-    ymax = ur.get(1)
+	
+	'''
+	Return ee.ImaceCollection of polygons used to submit full res queries
+	'''
+	
+	bounds = ee.Geometry(geometry).bounds()
+	coords = ee.List(bounds.coordinates().get(0))
+	ll = ee.List(coords.get(0))
+	ur = ee.List(coords.get(2))
+	xmin = ll.get(0)
+	xmax = ur.get(0)
+	ymin = ll.get(1)
+	ymax = ur.get(1)
 
-    latlist = ee.List.sequence(ymin, ymax, dx)
-    lonlist = ee.List.sequence(xmin, xmax, dy)
-    
-    polys = []
-    
-    for lon in lonlist.getInfo():
-        for lat in latlist.getInfo():
-        
-            def make_rect(lat, lon):
-                lattemp = ee.Number(lat)
-                lontemp = ee.Number(lon)
-                uplattemp = lattemp.add(dy)
-                lowlontemp = lontemp.add(dx)
+	latlist = ee.List.sequence(ymin, ymax, dx)
+	lonlist = ee.List.sequence(xmin, xmax, dy)
+	
+	polys = []
+	
+	for lon in lonlist.getInfo():
+		for lat in latlist.getInfo():
+		
+			def make_rect(lat, lon):
+				lattemp = ee.Number(lat)
+				lontemp = ee.Number(lon)
+				uplattemp = lattemp.add(dy)
+				lowlontemp = lontemp.add(dx)
 
-                return ee.Feature(ee.Geometry.Polygon([[lontemp, lattemp],[lowlontemp, lattemp],[lowlontemp, uplattemp],[lontemp, uplattemp]]))
-            
-            poly = make_rect(lat,lon)
-            polys.append(poly)
-    
-    return ee.FeatureCollection(ee.List(polys))
+				return ee.Feature(ee.Geometry.Polygon([[lontemp, lattemp],[lowlontemp, lattemp],[lowlontemp, uplattemp],[lontemp, uplattemp]]))
+			
+			poly = make_rect(lat,lon)
+			polys.append(poly)
+	
+	return ee.FeatureCollection(ee.List(polys))
 
 def chunk(dataset,area):
-    '''
-    This doesnt work yet as expected. This sums the variable in each chunk to give a pseudospatial respresentation 
-    '''
-    polys = gen_polys(area)
-    d = polys.getInfo()
-    results = []
-    for i in d['features']:
-        aoi = ee.Geometry.Polygon(i['geometry']['coordinates']).intersection(area)
-        t = col.filter(ee.Filter.calendarRange(year, year, 'year')).filter(ee.Filter.calendarRange(month, month, 'month')).select(var).filterBounds(aoi).sum()
-        t2 = t.multiply(1e-3).multiply(ee.Image.pixelArea()).multiply(scaling_factor).multiply(1e-9)
+	'''
+	This doesnt work yet as expected. This sums the variable in each chunk to give a pseudospatial respresentation 
+	'''
+	polys = gen_polys(area)
+	d = polys.getInfo()
+	results = []
+	for i in d['features']:
+		aoi = ee.Geometry.Polygon(i['geometry']['coordinates']).intersection(area)
+		t = col.filter(ee.Filter.calendarRange(year, year, 'year')).filter(ee.Filter.calendarRange(month, month, 'month')).select(var).filterBounds(aoi).sum()
+		t2 = t.multiply(1e-3).multiply(ee.Image.pixelArea()).multiply(scaling_factor).multiply(1e-9)
 
-        scale = t2.projection().nominalScale()
-        sumdict  = t2.reduceRegion(
-            reducer = ee.Reducer.sum(),
-            geometry = aoi,
-            scale = scale)
+		scale = t2.projection().nominalScale()
+		sumdict  = t2.reduceRegion(
+			reducer = ee.Reducer.sum(),
+			geometry = aoi,
+			scale = scale)
 
-        result = sumdict.getInfo()[var]
-        results.append(result)
-        print('poly complete')
-        
-    return results
+		result = sumdict.getInfo()[var]
+		results.append(result)
+		print('poly complete')
+		
+	return results
 
-def calc_monthly_sum(dataset, years, months):
-    
-    '''
-    Calculates monthly sum for hourly data. works for GLDAS / NLDAS 
-    '''
-    ImageCollection = dataset[0]
-    var = dataset[1]
-    scaling_factor = dataset[2]
-            
-    period_start = datetime.datetime(years[0], 1, 1)
-    start_date = period_start.strftime("%Y-%m-%d")
-    period_end = datetime.datetime(years[-1]+1, 1, 1)
-    dt_idx = pd.date_range(period_start,period_end, freq='M')
-    
-    sums = []
-    seq = ee.List.sequence(0, len(dt_idx))
-    
-    for i in seq.getInfo():
-        start = ee.Date(start_date).advance(i, 'month')
-        end = start.advance(1, 'month');
-        im = ee.ImageCollection(ImageCollection).select(var).filterDate(start, end).sum().set('system:time_start', start.millis())
-        ic = im.multiply(1e-3).multiply(ee.Image.pixelArea()).multiply(scaling_factor).multiply(1e-9)
-        scale = ic.projection().nominalScale()
-        
-        sumdict  = ic.reduceRegion(
-            reducer = ee.Reducer.sum(),
-            geometry = area,
-            scale = scale)
-        total = sumdict.getInfo()[var]
-        print(total)
-        sums.append(total)
+def calc_monthly_sum(dataset, years, months, area):
+	
+	'''
+	Calculates monthly sum for hourly data. works for GLDAS / NLDAS 
+	'''
+	ImageCollection = dataset[0]
+	var = dataset[1]
+	scaling_factor = dataset[2]
+			
+	period_start = datetime.datetime(years[0], 1, 1)
+	start_date = period_start.strftime("%Y-%m-%d")
+	period_end = datetime.datetime(years[-1]+1, 1, 1)
+	dt_idx = pd.date_range(period_start,period_end, freq='M')
+	
+	sums = []
+	seq = ee.List.sequence(0, len(dt_idx))
+	
+	for i in seq.getInfo():
+		start = ee.Date(start_date).advance(i, 'month')
+		end = start.advance(1, 'month');
+		im = ee.ImageCollection(ImageCollection).select(var).filterDate(start, end).sum().set('system:time_start', start.millis())
+		ic = im.multiply(1e-3).multiply(ee.Image.pixelArea()).multiply(scaling_factor).multiply(1e-9)
+		scale = ic.projection().nominalScale()
+		
+		sumdict  = ic.reduceRegion(
+			reducer = ee.Reducer.sum(),
+			geometry = area,
+			scale = scale)
+		total = sumdict.getInfo()[var]
+		print(total)
+		sums.append(total)
 
-    return sums
+	return sums
+
+def img_to_arr(eeImage, var_name):
+	temp = eeImage.select(var_name).clip(area)
+	latlon = eeImage.pixelLonLat().addBands(temp)
+	
+	latlon = latlon.reduceRegion(
+		reducer = ee.Reducer.toList(),
+		geometry = area, 
+		scale = 1000
+		)
+	
+	data = np.array((ee.Array(latlon.get(var_name)).getInfo()))
+	lats = np.array((ee.Array(latlon.get('latitude')).getInfo()))
+	lons = np.array((ee.Array(latlon.get('longitude')).getInfo()))
+	
+	lc,freq = np.unique(data,return_counts = True)
+	
+	return data, lats,lons 
+
+def imc_to_arr(eeImage):
+	temp = eeImage.filterBounds(area).first().pixelLonLat()
+	
+	latlon = temp.reduceRegion(
+		reducer = ee.Reducer.toList(),
+		geometry = area, 
+		scale = 1000
+		)
+	
+	data = np.array((ee.Array(latlon.get('cropland')).getInfo()))
+	lats = np.array((ee.Array(latlon.get('latitude')).getInfo()))
+	lons = np.array((ee.Array(latlon.get('longitude')).getInfo()))
+	
+	lc,freq = np.unique(data,return_counts = True)
+	
+	return data, lats,lons 
+
+def arr_to_img(data,lats,lons):
+	uniquelats = np.unique(lats)
+	uniquelons = np.unique(lons)
+	
+	ncols = len(uniquelons)
+	nrows = len(uniquelats)
+	
+	ys = uniquelats[1] - uniquelats[0]
+	xs = uniquelons[1] - uniquelons[0]
+	
+	arr = np.zeros([nrows, ncols], np.float32)
+	
+	counter = 0
+	for y in range(0, len(arr),1):
+		for x in range(0, len(arr[0]),1):
+			if lats[counter] == uniquelats[y] and lons[counter] == uniquelons[x] and counter < len(lats)-1:
+				counter+=1
+				arr[len(uniquelats)-1-y,x] = data[counter]
+				
+	return arr
+
+def freq_hist(eeImage, area, var_name):    
+	freq_dict = ee.Dictionary(
+	  eeImage.reduceRegion(ee.Reducer.frequencyHistogram(), area, 30).get(var_name)
+	);
+	
+	return freq_dict
+
 
 
 def load_data():
@@ -207,14 +283,14 @@ def load_data():
 	data['prism'] = [ee.ImageCollection("OREGONSTATE/PRISM/AN81m"), "ppt", 1]
 	data['chirps'] = [ee.ImageCollection('UCSB-CHG/CHIRPS/PENTAD'), "precipitation", 1]
 	data['persia'] = [ee.ImageCollection("NOAA/PERSIANN-CDR"), "precipitation", 1]
-
-	# https://developers.google.com/earth-engine/datasets/catalog/NASA_ORNL_DAYMET_V3
 	data['dmet'] = [ee.ImageCollection('NASA/ORNL/DAYMET_V3'), "prcp", 1]
 
 	###################
 	##### SWE data #####
 	####################
-	data['swe'] = [ee.ImageCollection('NASA/FLDAS/NOAH01/C/GL/M/V001'), "SWE_inst", 1 ]
+	data['fldas_swe'] = [ee.ImageCollection('NASA/FLDAS/NOAH01/C/GL/M/V001'), "SWE_inst", 1 ]
+	data['gldas_swe'] = [ee.ImageCollection('NASA/GLDAS/V021/NOAH/G025/T3H'), "SWE_inst", 1 / 240 ]
+	data['dmet_swe'] = [ee.ImageCollection('NASA/ORNL/DAYMET_V3'), "swe", 1]
 
 	####################
 	##### DS data ######
@@ -224,19 +300,112 @@ def load_data():
 	####################
 	##### R data #######
 	####################
-	data['r'] = [ee.ImageCollection('IDAHO_EPSCOR/TERRACLIMATE'), "ro", 1]
-	data['runoff'] = [ee.ImageCollection("NASA/FLDAS/NOAH01/C/GL/M/V001"), "Qs_tavg", 86400*24]
+	data['tc_r'] = [ee.ImageCollection('IDAHO_EPSCOR/TERRACLIMATE'), "ro", 1]
+	data['fldas_r'] = [ee.ImageCollection("NASA/FLDAS/NOAH01/C/GL/M/V001"), "Qs_tavg", 86400*24]
+
+	# GLDAS
+	data['ssr'] = [ee.ImageCollection('NASA/GLDAS/V021/NOAH/G025/T3H'), "Qs_acc", 1]
+	data['bfr'] = [ee.ImageCollection('NASA/GLDAS/V021/NOAH/G025/T3H'), "Qsb_acc", 1]
+	data['qsm'] = [ee.ImageCollection('NASA/GLDAS/V021/NOAH/G025/T3H'), "Qsm_acc", 1]
 
 	#####################
 	##### SM data #######
 	#####################
+	data['tc_sm'] = [ee.ImageCollection('IDAHO_EPSCOR/TERRACLIMATE'), "soil", 0.1]
 
-	data['sm'] = [ee.ImageCollection('IDAHO_EPSCOR/TERRACLIMATE'), "soil", 0.1]
+	data['sm1'] = [ee.ImageCollection("NASA/FLDAS/NOAH01/C/GL/M/V001"), "SoilMoi00_10cm_tavg", 1 ]
+	data['sm2'] = [ee.ImageCollection("NASA/FLDAS/NOAH01/C/GL/M/V001"), "SoilMoi10_40cm_tavg", 1 ]
+	data['sm3'] = [ee.ImageCollection("NASA/FLDAS/NOAH01/C/GL/M/V001"), "SoilMoi40_100cm_tavg", 1 ]
+	data['sm4'] = [ee.ImageCollection("NASA/FLDAS/NOAH01/C/GL/M/V001"), "SoilMoi100_200cm_tavg", 1 ]
 
-	data['sm1'] = [ee.ImageCollection("NASA/FLDAS/NOAH01/C/GL/M/V001"), "SoilMoi00_10cm_tavg", 1]
-	data['sm2'] = [ee.ImageCollection("NASA/FLDAS/NOAH01/C/GL/M/V001"), "SoilMoi10_40cm_tavg", 1]
-	data['sm3'] = [ee.ImageCollection("NASA/FLDAS/NOAH01/C/GL/M/V001"), "SoilMoi40_100cm_tavg", 1]
-	data['sm4'] = [ee.ImageCollection("NASA/FLDAS/NOAH01/C/GL/M/V001"), "SoilMoi100_200cm_tavg", 1]
+	data['gsm1'] = [ee.ImageCollection("NASA/GLDAS/V021/NOAH/G025/T3H"), "SoilMoi0_10cm_inst", 1/240 ]
+	data['gsm2'] = [ee.ImageCollection("NASA/GLDAS/V021/NOAH/G025/T3H"), "SoilMoi10_40cm_inst", 1/240 ]
+	data['gsm3'] = [ee.ImageCollection("NASA/GLDAS/V021/NOAH/G025/T3H"), "SoilMoi40_100cm_inst", 1/240 ]
+	data['gsm4'] = [ee.ImageCollection("NASA/GLDAS/V021/NOAH/G025/T3H"), "SoilMoi100_200cm_inst", 1/240 ]
 
 	return data
 
+def cdl_2_faunt():
+	
+	'''
+	Now: Classify crop types from CDL to the faunt (2009), schmid (2004) scheme 
+
+	CDL classes: https://developers.google.com/earth-engine/datasets/catalog/USDA_NASS_CDL
+	Faunt kc and classes: https://water.usgs.gov/GIS/metadata/usgswrd/XML/pp1766_fmp_parameters.xml 
+
+	Dict Key is the Faunt class (int)     
+	Dict Value is the CDL category (string)
+
+	The faunt class = CDL category is shown at the top of each k:v pair. 
+	'''
+	
+	data = {
+	# Water = water(83), wetlands(87), Aquaculture(92), Open Water(111), Perreniel Ice / Snow (112)
+	1 : ["83", "87", "92", "111", "112"], 
+	# Urban = developed high intensity(124), developed medium intensity(123)
+	2 : ["124", "123"], 
+	# Native = grassland/pasture(176), Forest(63), Shrubs(64), barren(65, 131), Clover/Wildflowers(58)
+	# Forests (141 - 143), Shrubland (152)
+	3 : ["176","63","64", "65", "131","58", "141", "142", "143", "152"], 
+	# Orchards, groves, vineyards = 
+	4 : [""],
+	# Pasture / hay = other hay / non alfalfa (37)
+	5 : ["37"],
+	# Row Crops = corn (1), soybeans (5),Sunflower(6) sweet corn (12), pop corn (13), double winter/corn (225), 
+	# double oats/corn(226), double barley/corn(237), double corn / soybeans
+	6 : ["1", "5", "6", "12", "13", "225", "226", "237", "239"] ,
+	# Small Grains = Spring wheat (23), winter wheat (24), other small grains (25), winter wheat / soybeans (26), 
+	# rye (27), oats (28), Millet(29), dbl soybeans/oats(240)
+	7 : ["23", "24", "25", "26", "27", "28", "29", "240"] ,
+	# Idle/fallow = Sod/Grass Seed (59), Fallow/Idle Cropland(61), 
+	8 : ["59","61"],
+	# Truck, nursery, and berry crops = 
+	# Blueberries (242), Cabbage(243), Cauliflower(244), celery (245), radishes (246), Turnips(247)
+	# Eggplants (249), Cranberries (250), Caneberries (55), Brocolli (214), Peppers(216), 
+	# Greens(219), Strawberries (221), Lettuce (227), Double Lettuce/Grain (230 - 233)
+	9 : ["242", "243", "244", "245", "246", "247", "248", "249", "250", "55", "214", "216","219","221", "227", "230", "231", "232", "233"], 
+
+	# Citrus and subtropical = Citrus(72), Oranges (212), Pommegranates(217)
+	10 : ["72", "212", "217"] ,
+
+	# Field Crops = 
+	# Peanuts(10),Mint (14),Canola (31),  Vetch(224),  Safflower(33) , RapeSeed(34), 
+	# Mustard(35) Alfalfa (36),Camelina (38), Buckwheat (39), Sugarbeet (41), Dry beans (42), Potaoes (43)
+	# Sweet potatoes(46), Misc Vegs & Fruits (47), Cucumbers(50)
+	# Chick Peas(51),Lentils(52),Peas(53),Tomatoes(54)Hops(56),Herbs(57),Carrots(206),
+	# Asparagus(207),Garlic(208), Cantaloupes(209), Honeydew Melons (213), Squash(222), Pumpkins(229), 
+
+	11 : ["10",  "14", "224", "31","33", "34", "35", "36", "38", "39", "41", "42", "43", "46", "47", "48" ,
+		  "49", "50", "51", "52", "53", "54",  "56", "57","206","207", "208", "209","213","222", "229"] ,
+
+	# Vineyards = Grapes(69)
+	12 : ["69"],
+	# Pasture = Switchgrass(60)
+	13 : ["60"],
+	# Grain and hay = Sorghum(4), barley (21), Durham wheat (22), Triticale (205), 
+	# Dbl grain / sorghum (234 - 236), Dbl 
+	14 : ["4", "21", "22", "205", "234", "235", "236"],
+	# livestock feedlots, diaries, poultry farms = 
+	15 : [""],
+
+	# Deciduous fruits and nuts = Pecans(74), Almonds(75), 
+	# Walnuts(76), Cherries (66), Pears(77), Apricots (223), Apples (68), Christmas Trees(70)
+	# Prunes (210), Plums (220), Peaches(67), Other Tree Crops (71), Pistachios(204), 
+	# Olives(211), Nectarines(218), Avocado (215)
+	16 : ["74", "75", "76","66","77", "223", "68", "210", "220", "67", "70", "71", "204", "211","215","218"],
+
+	# Rice = Rice(3)
+	17 : ["3"],
+	# Cotton = Cotton (2) , Dbl grain / cotton (238-239)
+	18 : ["2", "238", "239"], 
+	# Developed = Developed low intensity (122) developed open space(121)
+	19 : ["122", "121"],
+	# Cropland and Pasture
+	20 : [""],
+	# Cropland = Other crops (44)
+	21 : ["44"], 
+	# Irrigated row and field crops = Woody Wetlands (190), Herbaceous wetlands = 195
+	22 : ["190", "195"] 
+	}
+	
+	return data
