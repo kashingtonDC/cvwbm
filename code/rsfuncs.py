c294b82b9110f13a193197f19c1a2cdca2baec50
Aakash Ahamed <ahamednasa@gmail.com>
2019-08-26 11:42:34 -0700

    fix indentation in rsfuncs

--- code/rsfuncs.py
+++ code/rsfuncs.py
@@ -36,16 +36,16 @@ def gdf_to_ee_multipoly(gdf):
 	return ee.Geometry.MultiPolygon(multipoly)
 
 def get_area(gdf, fast = True):
-    
-    t = gdf.buffer(0.001).unary_union
-    d  = gp.GeoDataFrame(geometry=gp.GeoSeries(t))
-    if fast:
-        d2  = gp.GeoDataFrame(geometry=gp.GeoSeries(d.simplify(0.001))) 
-        area = gdf_to_ee_multipoly(d2)
-    else:
-        area = gdf_to_ee_multipoly(d)
-        
-    return area
+	
+	t = gdf.buffer(0.001).unary_union
+	d  = gp.GeoDataFrame(geometry=gp.GeoSeries(t))
+	if fast:
+		d2  = gp.GeoDataFrame(geometry=gp.GeoSeries(d.simplify(0.001))) 
+		area = gdf_to_ee_multipoly(d2)
+	else:
+		area = gdf_to_ee_multipoly(d)
+		
+	return area
 
 
 
@@ -72,15 +72,15 @@ def get_data(dataset, year, month, area):
 	return result
 
 def col_to_dt(df):
-    '''
-    converts the first col of a dataframe read from CSV to datetime
-    '''
-    t = df.copy()
-    t['dt'] = pd.to_datetime(df[df.columns[0]])
-    t = t.set_index(pd.to_datetime(t[t.columns[0]]))
-    t.drop([t.columns[0], "dt"],axis = 1, inplace = True)
-    
-    return t
+	'''
+	converts the first col of a dataframe read from CSV to datetime
+	'''
+	t = df.copy()
+	t['dt'] = pd.to_datetime(df[df.columns[0]])
+	t = t.set_index(pd.to_datetime(t[t.columns[0]]))
+	t.drop([t.columns[0], "dt"],axis = 1, inplace = True)
+	
+	return t
 
 def monthly_sum(dataset, years, months, area):
 	
@@ -187,7 +187,7 @@ def calc_monthly_sum(dataset, years, months, area):
 			reducer = ee.Reducer.sum(),
 			geometry = area,
 			scale = scale,
-            bestEffort = True)
+			bestEffort = True)
 		total = sumdict.getInfo()[var]
 		print(total)
 		sums.append(total)
@@ -324,8 +324,8 @@ def load_data():
 	##### SM data #######
 	#####################
 	data['tc_sm'] = [ee.ImageCollection('IDAHO_EPSCOR/TERRACLIMATE'), "soil", 0.1]
-    
-    data['gldas_sm'] = [ee.ImageCollection('NASA/GLDAS/V021/NOAH/G025/T3H'), "RootMoist_inst", 1 / 240]
+	
+	data['gldas_sm'] = [ee.ImageCollection('NASA/GLDAS/V021/NOAH/G025/T3H'), "RootMoist_inst", 1 / 240]
 
 	data['sm1'] = [ee.ImageCollection("NASA/FLDAS/NOAH01/C/GL/M/V001"), "SoilMoi00_10cm_tavg", 1 ]
 	data['sm2'] = [ee.ImageCollection("NASA/FLDAS/NOAH01/C/GL/M/V001"), "SoilMoi10_40cm_tavg", 1 ]
